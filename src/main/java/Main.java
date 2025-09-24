351b3a652f797a86f6a999932b72ef5efe83181a
mechim <michael.echim@gmail.com>
2025-09-24 13:22:38 +0300

    stream the input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        List<String> data = Utils.fetchData();
-        List<List<String>> result = getAnagrams(data);
+        List<List<String>> result = getAnagrams();
         Utils.printResponse(result);
     }
 
@@ -25,22 +28,27 @@ public class Main {
         return hash.toString();
     }
 
-    static List<List<String>> getAnagrams(List<String> words){
+    static List<List<String>> getAnagrams(){
         List<List<String>> anagramGroups = new ArrayList<>();
         Map<String, Integer> mp = new HashMap<>();
 
-        int wordsLength = words.size();
-        for (int i = 0; i < wordsLength; i++){
-            String currentWord = words.get(i);
-            String key = getHash(currentWord);
-
-            if (!mp.containsKey(key)){
-                mp.put(key, anagramGroups.size());
-                anagramGroups.add(new ArrayList<>());
+        try(InputStream inputStream = Utils.class.getResourceAsStream("/sample.txt");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))){
+            String currentWord;
+            while ((currentWord = bufferedReader.readLine())!= null){
+                String key = getHash(currentWord);
+                if (!mp.containsKey(key)){
+                    mp.put(key, anagramGroups.size());
+                    anagramGroups.add(new ArrayList<>());
+                }
+                anagramGroups.get(mp.get(key)).add(currentWord);
             }
-
-            anagramGroups.get(mp.get(key)).add(words.get(i));
+        } catch (IOException e){
+            System.err.println("Error reading file " + e.getMessage());
         }
+
+
+
 
         return anagramGroups;
     }
